bcf186220c4e4d87cc22fcc1215bf6b4f64b92b6
Vasenina Yuliya <mamicheva@gmail.com>
2022-01-06 23:53:24 +0100

    heroku

--- server.js
+++ server.js
@@ -132,4 +132,6 @@ app.get("*", (req, res) => {
     res.sendFile(`${__dirname}/index.html`);
 });
 
-app.listen(8080, () => console.log(`I'm listening on 8080...`));
+app.listen(process.env.PORT || 8080, () => {
+    console.log("Imageboard server is listening on 8080..");
+});
